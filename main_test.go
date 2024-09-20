0f37d6350c11679cfd8dee9cccaabd3fc4373524
oThinas <thiagomartinsprado@gmail.com>
2024-09-20 10:28:02 -0300

    test: add test for endpoints

--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"gin-rest-api/controllers"
 	"gin-rest-api/database"
 	"gin-rest-api/models"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -34,6 +36,10 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/api/students", controllers.GetAllStudents)
 	r.GET("/api/students/cpf/:cpf", controllers.GetStudentByCPF)
+	r.GET("/api/students/:id", controllers.GetStudentById)
+	r.POST("/api/students", controllers.CreateStudent)
+	r.PUT("/api/students/:id", controllers.UpdateStudent)
+	r.DELETE("/api/students/:id", controllers.DeleteStudentById)
 
 	return r
 }
@@ -73,6 +79,106 @@ func TestGetStudentByCPF(t *testing.T) {
 	r.ServeHTTP(response, request)
 
 	assert.Equal(t, http.StatusOK, response.Code)
+
+	var studentResponse models.Student
+	err := json.Unmarshal(response.Body.Bytes(), &studentResponse)
+	assert.NoError(t, err)
+
+	assert.Equal(t, student.Name, studentResponse.Name)
+	assert.Equal(t, student.CPF, studentResponse.CPF)
+	assert.Equal(t, student.RG, studentResponse.RG)
+}
+
+func TestGetStudentById(t *testing.T) {
+	database.Connect()
+	r := SetupRouter()
+	student := newStudentMock()
+	createStudentMock(&student)
+	defer deleteStudentMock()
+
+	request, _ := http.NewRequest("GET", "/api/students/"+strconv.FormatUint(uint64(studentId), 10), nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, request)
+
+	assert.Equal(t, http.StatusOK, response.Code)
+
+	var studentResponse models.Student
+	err := json.Unmarshal(response.Body.Bytes(), &studentResponse)
+	assert.NoError(t, err)
+
+	assert.Equal(t, student.Name, studentResponse.Name)
+	assert.Equal(t, student.CPF, studentResponse.CPF)
+	assert.Equal(t, student.RG, studentResponse.RG)
+}
+
+func TestDeleteStudentById(t *testing.T) {
+	database.Connect()
+	r := SetupRouter()
+	student := newStudentMock()
+	createStudentMock(&student)
+
+	request, _ := http.NewRequest("DELETE", "/api/students/"+strconv.FormatUint(uint64(studentId), 10), nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, request)
+
+	assert.Equal(t, http.StatusOK, response.Code)
+
+	var responseBody map[string]interface{}
+	err := json.Unmarshal(response.Body.Bytes(), &responseBody)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "Student deleted successfully!", responseBody["message"])
+
+	deletedStudent := responseBody["student"].(map[string]interface{})
+	assert.Equal(t, student.Name, deletedStudent["name"])
+	assert.Equal(t, student.CPF, deletedStudent["cpf"])
+	assert.Equal(t, student.RG, deletedStudent["rg"])
+}
+
+func TestUpdateStudent(t *testing.T) {
+	database.Connect()
+	r := SetupRouter()
+	student := newStudentMock()
+	createStudentMock(&student)
+	defer deleteStudentMock()
+
+	studentUpdated := student
+	studentUpdated.Name = "Updated"
+	studentUpdatedJson, _ := json.Marshal(studentUpdated)
+	request, _ := http.NewRequest(
+		"PUT",
+		"/api/students/"+strconv.FormatUint(uint64(studentId), 10),
+		bytes.NewBuffer(studentUpdatedJson),
+	)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, request)
+
+	assert.Equal(t, http.StatusOK, response.Code)
+
+	var studentResponse models.Student
+	err := json.Unmarshal(response.Body.Bytes(), &studentResponse)
+	assert.NoError(t, err)
+
+	assert.Equal(t, studentUpdated.Name, studentResponse.Name)
+	assert.Equal(t, studentUpdated.CPF, studentResponse.CPF)
+	assert.Equal(t, studentUpdated.RG, studentResponse.RG)
+}
+
+func TestCreateStudent(t *testing.T) {
+	database.Connect()
+	r := SetupRouter()
+	student := newStudentMock()
+
+	studentJson, _ := json.Marshal(student)
+	request, _ := http.NewRequest(
+		"POST",
+		"/api/students",
+		bytes.NewBuffer(studentJson),
+	)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, request)
+
+	assert.Equal(t, http.StatusCreated, response.Code)
 
 	var studentResponse models.Student
 	err := json.Unmarshal(response.Body.Bytes(), &studentResponse)
